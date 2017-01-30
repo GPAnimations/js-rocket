c5ef15f3ef21ba9a3e39f22474617bfe5f24f36b
gpanimations <gregpugh@gpanimations.com>
2017-01-30 06:07:32 -0500

    Update HTML

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
 var favColor = 'blue';
 var myFavColors = ['blue', 'red', 'green'];
-var numOfFavColors = '3';
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Greg',
+	lastName: 'Pugh',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 67,
+};
+
+var whatIsMyFavColor = function () {
+	return 'black';
+}
+
+
+
+var someFunction = function() {
+	console.log ("Returning Info");
+}
